200fc3adae6026fcd4d2610fbff78a0906642a38
Joe <jblackman1609@gmail.com>
2026-08-13 21:23:50 -0500

    commit changes

--- BUGS/App.xaml.cs
+++ BUGS/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace BUGS;
 
@@ -9,5 +12,22 @@ namespace BUGS;
 /// </summary>
 public partial class App : Application
 {
+    public IConfiguration? Configuration { get; private set; }
+
+    public App()
+    {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        Configuration = builder.Build();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
 }
 
