f01ba5e7caf1024b0e16efb5c4ac1e99c2934361
Joe Blackman <jblackman1609@gmail.com>
2026-08-17 17:08:48 -0500

    added appsettings.json

--- BUGS/App.xaml.cs
+++ BUGS/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Windows;
+using BUGS.Windows;
+using Microsoft.Extensions.Configuration;
 
 namespace BUGS;
 
@@ -9,5 +13,19 @@ namespace BUGS;
 /// </summary>
 public partial class App : Application
 {
+    [Required]
+    public IConfiguration Configuration { get; private set; }
+
+    public App()
+    {
+        var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", false, true);
+
+        Configuration = builder.Build();
+
+        MainWindow mainWindow = new();
+        mainWindow.Show();
+    }
 }
 
