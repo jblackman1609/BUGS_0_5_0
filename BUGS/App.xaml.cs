691e08fe21721bf0cf1ad8da9f4c939f19f7afcf
Joe Blackman <jblackman1609@gmail.com>
2026-08-17 06:30:52 -0500

    It Runs...

--- BUGS/App.xaml.cs
+++ BUGS/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using BUGS.Windows;
 
 namespace BUGS;
 
@@ -9,5 +10,10 @@ namespace BUGS;
 /// </summary>
 public partial class App : Application
 {
+    public App()
+    {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.Show();
+    }
 }
 
